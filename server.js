9fb0e06745b2adb801bba923ffd62db8e8c2e1d4
Ariel Anivarro <anivarrorlando@hotmail.com>
2018-07-13 18:55:39 -0400

    sdffsd

--- server.js
+++ server.js
@@ -4,12 +4,11 @@ const path = require('path');
 
 const app = express();
 
-// app.use(express.static(path.join(__dirname, 'dist')));
-// const index = path.join(__dirname, 'build', 'index.html');
+app.use(express.static(path.join(__dirname, 'dist')));
+
 
 app.get('*', (req, res) => {
-  // res.sendFile(path.join(__dirname + '/dist/index.html'));
-  res.sendFile(path.join(__dirname, 'src', 'index.html'));
+  res.sendFile(path.join(__dirname + '/dist/siapec/index.html'));
 });
 
 const port = process.env.PORT || 9696;
