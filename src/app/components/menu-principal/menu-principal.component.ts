f88f8692daa28e7f1085e308622505f26e2b13e2
Ariel Anivarro <anivarrorlando@hotmail.com>
2018-07-13 18:23:31 -0400

    arielanivarro

---
+++ src/app/components/menu-principal/menu-principal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-principal',
+  templateUrl: './menu-principal.component.html'
+})
+export class MenuPrincipalComponent {
+
+  constructor() { }
+
+}
