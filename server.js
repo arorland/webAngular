14972f774e231335f3a2df06541eeaf7a6f443db
Ariel Anivarro <anivarrorlando@hotmail.com>
2018-07-13 18:39:32 -0400

    dsfsd

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.get('*', (req, res) => {
   // res.sendFile(path.join(__dirname + '/dist/index.html'));
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
 const port = process.env.PORT || 9696;
