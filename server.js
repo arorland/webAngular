5de2192af8fd83a0d4292322ad2cf862896cb0f8
Ariel Anivarro <anivarrorlando@hotmail.com>
2018-07-16 10:22:31 -0400

    sdf

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ constexpress=require('express');
 constapp=expres();
 constpath=require('path');
 
-app.us(express.static(__dirname+'/dist'));
+app.us(express.static(__dirname+'/src'));
 
 app.listen(process.env.PORT||3000);
 
 
 //PathLocationStradegy
 app.get('/'function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname+'/src/index.html'));
 });
 
 console.log('Console Listening');
