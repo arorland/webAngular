67d246b567437d9e25d0180acbd2fb65c0a9c991
Ariel Anivarro <anivarrorlando@hotmail.com>
2018-07-16 09:29:46 -0400

    ariel

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, '/dist')));
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/siapec/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 const port = process.env.PORT || 9696;
