5d0df95c4606acbf976ed967e9ba3559338a25fe
Ariel Anivarro <anivarrorlando@hotmail.com>
2018-07-16 09:51:28 -0400

    sdfsd

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 app.set('port', port);
 
 const server = http.createServer(app);
